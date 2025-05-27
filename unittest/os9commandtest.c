ec75f2cea2331941211d1c1d99dd2599c50b70e0
tim lindner <tlindner@macmess.org>
2025-05-26 20:41:38 -0700

    Added back mkdir command to test internal rbf format.

--- unittest/os9commandtest.c
+++ unittest/os9commandtest.c
@@ -24,7 +24,10 @@ void test_os9_command_format()
 	ec = _native_gs_size(nativepath, &size);	
 	ASSERT_EQUALS(0, ec);
 	ASSERT_EQUALS(65000*256, size);
-	
+
+	ec = _os9_makdir("test.dsk,/CMDS");
+	ASSERT_EQUALS(0, ec);
+		
 	ec = _native_close(nativepath);
 	ASSERT_EQUALS(0, ec);
 	
